d4c9ffa6ddfa93189ad2a04e22ab40b294b34dd4
Nurcholish Dwilestanto <nurcholishdwi@gmail.com>
2022-09-15 14:34:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+new Vue({
+  el:'#app',
+  data(){
+    return{
+      info:null
+    }
+  },
+  mounted(){
+    axios
+    .get('http://192.168.2.99:8008')
+    .then(response =>(this.info =response))
+  }
+})
+</script>
+
+<template>
+  <main>
+    <h1> FrondEnd</h1>
+  </main>
+  
+  <span>{{info}}</span>
+</template>
+
+<style scoped>
+</style>
